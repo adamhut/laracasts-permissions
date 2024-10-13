497a8f5a18c5ec7207f69c28543cdbdfe7217048
Jeremy McPeak <jwmcpeak@gmail.com>
2024-10-13 14:00:15 -0500

    episode 4

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -19,11 +19,11 @@
                         {{ __('Articles') }}
                     </x-nav-link>
 
-                    @can('admin-access')
+                    @if(Auth::user()->hasRole('admin'))
                     <x-nav-link :href="route('users.index')" :active="request()->routeIs('users.index')">
                         {{ __('Users') }}
                     </x-nav-link>
-                    @endcan
+                    @endif
                 </div>
             </div>
 
