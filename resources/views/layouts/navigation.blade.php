f25601dea75a3f051595a2d0dde371432c69090f
Jeremy McPeak <jwmcpeak@gmail.com>
2024-10-20 14:09:14 -0500

    episode 6

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -19,7 +19,7 @@
                         {{ __('Articles') }}
                     </x-nav-link>
 
-                    @role('admin')
+                    @role('adMIn')
                     <x-nav-link :href="route('users.index')" :active="request()->routeIs('users.index')">
                         {{ __('Users') }}
                     </x-nav-link>
